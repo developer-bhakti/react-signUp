d5d4bd012a3bd11e4bbcec6c739ae473061a58cf
purnale <bhaktipurnale89@gmail.com>
2025-02-28 12:51:57 +0530

    add

--- src/App.jsx
+++ src/App.jsx
@@ -41,7 +41,7 @@ const App = () => {
             value={singleTask}
             onChange={(e) => setSingleTask(e.target.value)}
           />
-          <button className="border-2 py-2 px-6 hover:bg-black hover:text-white">
+          <button className="border-3 py-2 px-6 hover:bg-black hover:text-white">
             Submit
           </button>
         </form>
